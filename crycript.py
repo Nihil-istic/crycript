bd4ffaa292a7600d26af24ccaa0df9dfce738f27
Nihil-istic <69171611+Nihil-istic@users.noreply.github.com>
2020-10-22 20:45:23 -0500

    Add files via upload
    Added argparser functionality, optimized code, added minimum password length (6), changed version key

--- crycript.py
+++ crycript.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from base64 import urlsafe_b64encode
 from getpass import getpass
 from os import remove, urandom
@@ -10,7 +11,6 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-
 # GLOBAL VARIABLES
 # str: encrypted file extension
 EXTENSION = '.cry'
@@ -21,9 +21,11 @@ SALT_SIZE = 16
 # b str: separator
 SEPARATOR = b';'
 # b str: crycript version
-VERSION = b'CRYCRIPT:2020.10.01'
+VERSION = b'CRYCRIPT:2020.10.22'
 # b str: version key (this will be changing between each version)
-VERSION_KEY = b'Nncbuoc8pfiXJlaGhZwTKGmYsSSo0SZAnUf_KBnPnE0='
+VERSION_KEY = b'xCHACSdlbmg56i212CYyZrvl2weByLedxt7i3KitWS0='
+# int: password length
+PASSWORD_LENGTH = 6
 
 
 # ERRORS
@@ -31,12 +33,13 @@ class Error(Exception): pass
 class PathError(Error): pass
 class PasswordError(Error): pass
 class VersionError(Error): pass
+class ArgumentError(Error): pass
 
 
 def get_key(salt=None):
     # Get password
     password = getpass('Enter your password: ')
-    if not password == getpass('Confirm password: '):
+    if not password == getpass('Confirm password: ') and password >= PASSWORD_LENGTH:
         raise PasswordError('Passwords do not match.')
 
     # Change password string to binary string
@@ -167,3 +170,26 @@ def decrypt(path):
 
     elif not path.endswith(EXTENSION):
         raise PathError(f'Decryption aborted: path is not encrypted.')
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Custom python Fernet symmetric encryption tool by Salvador BG')
+    parser.add_argument('path', help='path to file.')
+
+    action = parser.add_mutually_exclusive_group()
+    action.add_argument('-e', '--encrypt', help='encrypt a file.', action='store_true')
+    action.add_argument('-d', '--decrypt', help='decrypt a file.', action='store_true')
+
+    args = parser.parse_args()
+
+    if (args.encrypt and args.decrypt) or (not args.encrypt and not args.decrypt):
+        raise ArgumentError('You need to choose one action (either encrypt or decrypt).')
+
+    elif args.encrypt:
+        encrypt(args.path)
+
+    elif args.decrypt:
+        decrypt(args.path)
+
+    else:
+        raise ArgumentError('How the fuck did you ended here?')
