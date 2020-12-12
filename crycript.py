dbf306f4aeea3aa84ebfbc3359e4723dfe0a0ac7
Nihil-istic <69171611+Nihil-istic@users.noreply.github.com>
2020-12-12 22:36:14 +0000

    crycript 2020.12.12
    Added a default shebang line
    Added type hints to global variables and arguments of functions
    Modified STR_VERSION
    PASSWORD_LENGTH is now a tuple, with the interval for the password length
    Added FILENAME_LENGTH to set a fixed length to encrypted filenames
    Added BUFFER, now is possible to work with files bigger than system RAM
    Added STOP_SIGNAL, bytes to append to the end of a encrypted file
    Added confusion(), but what is it?
    Added get_filename(), now encrypted filenames are random strings
    Added confirm argument to get_key(), for confirming password
    Deleted salt, as does not increase security
    encrypt() and decrypt() no longer uses get_key, now a cipher (Fernet class) is required as an argument, path is not evaluated with isfile(), integrated timer and new structure to encrypted files, using new lines to define diferent parts (associated with BUFFER) so it can process files bigger than RAM
    Now encrypt and decrypt return a string containing old and new filenames, as well as a timer

--- crycript.py
+++ crycript.py
@@ -1,10 +1,11 @@
-#!shebang
+#!/usr/bin/env python
 
-from argparse import ArgumentParser
 from base64 import urlsafe_b64encode
 from getpass import getpass
-from os import remove, urandom
+from os import remove, listdir
 from os.path import isfile, abspath, dirname
+from secrets import choice
+from time import time
 
 from cryptography.fernet import Fernet
 from cryptography.fernet import InvalidToken
@@ -12,54 +13,67 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-# GLOBAL VARIABLES
-# str: encrypted file extension
-EXTENSION = '.cry'
-# int: key iterations
-PBKDF_ITERATIONS = 100_000
-# int: salt size
-SALT_SIZE = 16
-# b str: separator
-SEPARATOR = b';'
-# b str: crycript version
-VERSION = b'CRYCRIPT:2020.11.20'
-# str: DO NOT TOUCH THIS ONE, string version of VERSION
-STR_VERSION = str(VERSION)[2:-1]
-# b str: version key (this will be changing between each version)
-VERSION_KEY = b'O87XjL67TLJP3pNa-YRCDQuR2DYTHGiy67m__-JPRbM='
-# int: password length
-PASSWORD_LENGTH = 8
+# Encrypted file extension
+EXTENSION: str = '.cry'
+
+# Password iterations for key generation
+PBKDF_ITERATIONS: int = 100_000
+
+# Version of crycript
+VERSION: bytes = b'CRYCRIPT:2020.12.12'
+
+# String version, do not modify it
+STR_VERSION: str = VERSION.decode()
+
+# Interval for allowed password length
+PASSWORD_LENGTH: tuple = (8, 128)
+
+# Encryption filename length
+FILENAME_LENGTH: int = 8
+
+# Buffer size for file reading (1 = 1 byte), 1 Megabyte = 1_000_000
+BUFFER: int = 10_000_000
+
+# Stop signal (end of encrypted file)
+STOP_SIGNAL: bytes = b'EOF'
 
 
-def bye(text):
+def bye(text: str):  # Print text and exit
     print(text)
     raise SystemExit
 
 
-def get_key(salt=None):
+def confusion():  # Show confusion and exit
+    bye('How the fuck did you end up here?')
+
+
+def get_filename():  # Generate random filename (for encrypted file)
+    charset = 'ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklnmopqrstuvwxyz'
+    new_filename = ''.join([choice(charset) for _ in range(FILENAME_LENGTH)]) + EXTENSION
+    return new_filename
+
+
+def get_key(confirm: bool):  # Generate a key from a given password
     # Get password
     password = getpass('Enter your password: ')
 
     # Check for length of password
-    if len(password) < PASSWORD_LENGTH:
-        bye(f'Password must be at least {PASSWORD_LENGTH} characters.')
+    if len(password) < PASSWORD_LENGTH[0] or len(password) > PASSWORD_LENGTH[1]:
+        bye(f'Password must be between {PASSWORD_LENGTH[0]} and {PASSWORD_LENGTH[1]} characters')
 
-    # Check to see if there is no typo in password
-    if not password == getpass('Confirm password: '):
-        bye('Passwords do not match.')
+    # Password confirmation
+    if confirm:
+        if not password == getpass('Confirm password: '):
+            bye('Passwords do not match')
 
     # Change password string to binary string
     password = password.encode()
-
-    # Salt
-    if salt is None:
-        salt = urandom(SALT_SIZE)
 
     # PBKDF2 function
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA512(),
         length=32,
-        salt=salt,
+        salt=b'',
         iterations=PBKDF_ITERATIONS,
         backend=default_backend()
     )
@@ -70,126 +84,161 @@ def get_key(salt=None):
     # Delete password from memory
     del password
 
-    return salt, key
+    # Return generated key
+    return key
 
 
-def encrypt(path):
-    if isfile(path) and not path.endswith(EXTENSION):
-        # Always work with absolute path
-        path = abspath(path)
+def encrypt(path: str, cipher: Fernet):  # Main encryption function
+    if not path.endswith(EXTENSION):
+        # Get start time
+        clock_start = time()
 
-        # File name
-        path_filename = path[1 + len(dirname(path)):].encode()
+        # Path for directory containing the file
+        path_dir = dirname(path) + '/'
 
-        # Path for the encrypted file
-        path_encrypted = dirname(path) + '/' + str(Fernet.generate_key())[2:-1] + EXTENSION
+        # Original filename
+        path_filename_old = path[len(path_dir):]
 
-        # Get encryption key
-        salt, key = get_key()
+        while True:  # Make sure to generate a new and unique filename (to not overwrite files)
+            # New filename
+            path_filename_new = get_filename()
 
-        # Set the cipher
-        cipher = Fernet(key)
+            if path_filename_new not in listdir(path_dir):
+                break
 
-        # Set version key cipher
-        version_cipher = Fernet(VERSION_KEY)
+        # Encrypted version of original filename
+        path_filename_old_encrypted = cipher.encrypt(path_filename_old.encode())
 
-        # Read the original file and extract its contents
-        with open(path, 'rb') as raw_file:
-            contents = raw_file.read()
+        # Open new file (encrypted) to add version and original filename
+        with open(path_dir + path_filename_new, 'wb') as file_encrypted:
+            # Add crycript version
+            file_encrypted.write(VERSION)
+            file_encrypted.write(b'\n')
 
-        # Encrypt contents
-        contents = cipher.encrypt(contents)
-        path_filename = cipher.encrypt(path_filename)
+            # Encrypted filename
+            file_encrypted.write(path_filename_old_encrypted)
+            file_encrypted.write(b'\n')
 
-        # Version key encryption
-        salt = version_cipher.encrypt(salt)
-        contents = version_cipher.encrypt(contents)
-        path_filename = version_cipher.encrypt(path_filename)
+        # Open original file and read its contents to start filling encrypted file
+        with open(path, 'rb') as file_decrypted:
+            while True:
+                # Read a string stream with the size of BUFFER
+                part_decrypted = file_decrypted.read(BUFFER)
 
-        # Write a new encrypted file
-        with open(path_encrypted, 'wb') as encrypted_file:
-            encrypted_file.write(VERSION + SEPARATOR + salt + SEPARATOR + path_filename + SEPARATOR + contents)
+                # If part_decrypted is b'' that means we reached the end of the file
+                if part_decrypted == b'':
+                    break
 
-        # Delete contents (encrypted) from memory
-        del contents
+                # Encrypt contents
+                part_encrypted = cipher.encrypt(part_decrypted)
 
-        # Delete old file
+                # Open encrypted file in append mode to add new encrypted content
+                with open(path_dir + path_filename_new, 'ab') as file_encrypted:
+                    # Encrypted content
+                    file_encrypted.write(part_encrypted)
+                    file_encrypted.write(b'\n')
+
+        # After encrypting contents, append STOP_SIGNAL
+        with open(path_dir + path_filename_new, 'ab') as file_encrypted:
+            # End of file
+            file_encrypted.write(STOP_SIGNAL)
+            file_encrypted.write(b'\n')
+
+        # Delete old (original) file
         remove(path)
 
-    elif not isfile(path):
-        bye('Encryption aborted: path is not a file.')
+        # Get end time
+        clock_end = time()
+
+        # Return clock
+        return f'{path_filename_old} -> {path_filename_new} in {round(clock_end - clock_start, 4)} seconds'
 
     elif path.endswith(EXTENSION):
-        bye(f'Encryption aborted: path is already encrypted.')
+        bye('Aborted, file already encrypted')
+
+    else:
+        confusion()
 
 
-def decrypt(path):
-    if isfile(path) and path.endswith(EXTENSION):
-        # Always work with absolute path
-        path = abspath(path)
+def decrypt(path: str, cipher: Fernet):  # Main decryption function
+    if path.endswith(EXTENSION):
+        # Get start time
+        clock_start = time()
 
-        # Extract the encrypted content
-        with open(path, 'rb') as encrypted_file:
-            version, salt, path_decrypted, contents = encrypted_file.read().split(SEPARATOR)
+        # Path to directory
+        path_dir = dirname(path) + '/'
 
-        # Make sure encrypted file is compatible with crycript version
-        if version != VERSION:
-            bye(f'Your encrypted file version is not supported.')
+        # Original filename
+        path_filename_old = path[len(path_dir):]
 
-        # Delete version from memory
-        del version
+        # Open original (encrypted) file
+        with open(path, 'rb') as file_encrypted:
+            # Read version of the file encryption
+            version = file_encrypted.readline()[:-1]
 
-        # Set version key cipher
-        version_cipher = Fernet(VERSION_KEY)
+            # Check if versions match
+            if version != VERSION:
+                bye('Aborted, invalid crycript version')
 
-        # Version decrypt contents
-        salt = version_cipher.decrypt(salt)
-        contents = version_cipher.decrypt(contents)
-        path_decrypted = version_cipher.decrypt(path_decrypted)
+            # Delete version
+            del version
 
-        # Get decryption key
-        salt, key = get_key(salt)
+            path_filename_new_encrypted = file_encrypted.readline()[:-1]
 
-        # Delete salt from memory
-        del salt
+            # Try to decrypt encrypted filename
+            try:
+                path_filename_new = cipher.decrypt(path_filename_new_encrypted)
+            except InvalidToken:
+                # If InvalidToken is raised, key is not the correct one
+                bye('Aborted, invalid password')
 
-        # Set the cipher
-        cipher = Fernet(key)
+            # Start to decrypt contents
+            while True:
+                # Get encrypted part
+                part_encrypted = file_encrypted.readline()[:-1]
 
-        try:
-            # Decrypt contents
-            contents = cipher.decrypt(contents)
+                # If part is equal to STOP_SIGNAL, stop
+                if part_encrypted == STOP_SIGNAL:
+                    break
 
-            # Decrypt filename
-            path_decrypted = cipher.decrypt(path_decrypted)
+                # Try to decrypt part
+                try:
+                    part_decrypted = cipher.decrypt(part_encrypted)
+                except InvalidToken:
+                    # If InvalidToken is raised, the file was probably modified
+                    try:
+                        remove(path_dir + path_filename_new.decode())
+                    except FileNotFoundError:
+                        pass
+                    bye('Aborted, invalid password for part')
 
-        except InvalidToken:
-            bye('Decryption aborted: invalid password.')
+                # Append decrypted part to new file
+                with open(path_dir + path_filename_new.decode(), 'ab') as file_decrypted:
+                    file_decrypted.write(part_decrypted)
 
-        # Write a new decrypted file
-        path_decrypted = dirname(path) + '/' + path_decrypted.decode()
-        with open(path_decrypted, 'wb') as decrypted_file:
-            decrypted_file.write(contents)
-
-        # Delete contents (decrypted) from memory
-        del contents
-
-        # Delete encrypted file
+        # Remove old (encrypted) file
         remove(path)
 
-    elif not isfile(path):
-        bye(f'Decryption aborted: path is not a file.')
+        # Get end time
+        clock_end = time()
+
+        # Return clock
+        return f'{path_filename_old} -> {path_filename_new.decode()} in {round(clock_end - clock_start, 4)} seconds'
 
     elif not path.endswith(EXTENSION):
-        bye(f'Decryption aborted: path is not encrypted.')
+        bye('Aborted, file is not encrypted')
+    else:
+        confusion()
 
 
 if __name__ == '__main__':
+    from argparse import ArgumentParser
+
     # Set parser
     parser = ArgumentParser(description='Python symmetric encryption tool by Salvador BG')
 
     # Set version
-    parser.add_argument('-v', '--version', help='show version and exit', action='version', version=STR_VERSION)
+    parser.add_argument('--version', help='show version and exit', action='version', version=STR_VERSION)
 
     # Set path argument
     parser.add_argument('path', help='path to file')
@@ -202,20 +251,25 @@ if __name__ == '__main__':
     # Parse arguments
     args = parser.parse_args()
 
-    # Make sure only one argument is given
-    if (args.encrypt and args.decrypt) or (not args.encrypt and not args.decrypt):
-        bye('You need to choose one action (either encrypt or decrypt).')
+    # Set absolute path
+    args_path = abspath(args.path)
+
+    # The script is designed to only work with files
+    if not isfile(args_path):
+        bye('Path is not a file')
 
     # Encrypt action
-    elif args.encrypt:
-        encrypt(args.path)
-        bye('Encryption completed.')
+    if args.encrypt:
+        encryption_cipher = Fernet(get_key(True))
+        status = encrypt(args_path, encryption_cipher)
+        bye(status)
 
     # Decrypt action
     elif args.decrypt:
-        decrypt(args.path)
-        bye('Decryption completed.')
+        decryption_cipher = Fernet(get_key(False))
+        status = decrypt(args_path, decryption_cipher)
+        bye(status)
 
-    # What the fuck?
+    # Action not selected
     else:
-        bye('How the fuck did you end up here?')
+        bye('You need to enter at least one action [-e | -d]')
