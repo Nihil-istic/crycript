c03e8bb9a5b513422311c94699206652ec6a258a
Nihil-istic <69171611+Nihil-istic@users.noreply.github.com>
2021-01-12 02:49:36 +0000

    Update crycript installer.sh

--- crycript installer.sh
+++ crycript installer.sh
@@ -4,7 +4,7 @@ chmod 600 ~/.local/opt/crycript-main/sources/*
 chmod 700 ~/.local/opt/crycript-main/sources/crycript_main.py
 ln -s ~/.local/opt/crycript-main/sources/crycript_main.py ~/.local/opt/python-scripts/crycript
 echo '# Python Scripts' >> ~/.bashrc
-echo 'export PATH=$PATH":$HOME/.local/opt/python-scripts/' >> ~/.bashrc
+echo 'export PATH=$PATH":$HOME/.local/opt/python-scripts/"' >> ~/.bashrc
 mkdir -pv ~/.local/share/applications
 mv -vi ~/.local/opt/crycript-main/crycript.desktop ~/.local/share/applications/crycript.desktop
 chmod 700 ~/.local/share/applications/crycript.desktop
