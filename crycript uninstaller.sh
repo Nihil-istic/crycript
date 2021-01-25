71fcfc78b025cb20cf3832d2eb8e539eb299a0a3
Nihil-istic <69171611+Nihil-istic@users.noreply.github.com>
2021-01-25 00:39:34 +0000

    Delete crycript uninstaller.sh

--- crycript uninstaller.sh
+++
@@ -1,6 +0,0 @@
-rm -Ivr ~/.local/opt/crycript-main
-rm -v ~/.local/opt/python-scripts/crycript
-rmdir -v ~/.local/opt/python-scripts
-rmdir -v ~/.local/opt
-rm -v ~/.local/share/applications/crycript.desktop
-echo "Remember to delete the line: export PATH=$PATH:$HOME/.local/opt/python-scripts/ in your .bashrc if no longer needed"
